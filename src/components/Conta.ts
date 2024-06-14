18b042eb23e042efdcb1f636bd72ed4b29da6ca1
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-14 16:08:47 -0300

    fist commit1

---
+++ src/components/Conta.ts
@@ -0,0 +1,13 @@
+import { Transacao } from "../types/Transacao";
+
+export class Conta{
+    nome: string
+    saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+    transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"),(key: string, value: any) =>{
+        if(key ==="data"){
+            return new Date(value);
+        }
+        return value;
+    }) || [];
+    
+}
